4bc5330d85b7dd9e04ccd83a94d52d19ea6c2394
shravansahan <shravansahan@gmail.com>
2023-11-20 15:33:49 +0530

    Changed project Name

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,6 +1,6 @@
 def call(String dockerHubUsername, String imageName) {
     // Build the Docker image
-    sh "docker build --build-arg REACT_APP_RAPID_API_KEY=f0ead79813ms -t ${imageName} ."
+    sh "docker build --build-arg REACT_APP_RAPID_API_KEY=52fd0087f3msh5bf44f904c201f6p151f5fjsn519ad3c25917 -t ${imageName} ."
      // Tag the Docker image
     sh "docker tag ${imageName} ${dockerHubUsername}/${imageName}:latest"
     // Push the Docker image
